279bcaa6cdd1c7bbc1350d84e5958db482619b3a
jackyjaiswal123 <55536245+jackyjaiswal123@users.noreply.github.com>
2021-04-12 13:26:43 +0530

    Update connect.php

--- conection/connect.php
+++ conection/connect.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect("localhost","root", "") or die("No Connection");
-mysql_select_db("project") or die("No Database connected!");
+mysql_connect("remotemysql.com","fqpcD1WsmT", "Xt9SLaf4VJ") or die("No Connection");
+mysql_select_db("fqpcD1WsmT") or die("No Database connected!");
 ?>
