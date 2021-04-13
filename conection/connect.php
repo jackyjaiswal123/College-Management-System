86601814bfbda1bcaba75b2b2702d4759b62dcc5
jackyjaiswal123 <55536245+jackyjaiswal123@users.noreply.github.com>
2021-04-13 08:48:24 +0530

    Update connect.php

--- conection/connect.php
+++ conection/connect.php
@@ -2,5 +2,6 @@
 //mysql_connect("remotemysql.com","fqpcD1WsmT", "Xt9SLaf4VJ") or die("No Connection");
 //mysql_select_db("fqpcD1WsmT") or die("No Database connected!");
 //$var = mysqli_connect('remotemysql.com', 'Xt9SLaf4VJ', 'fqpcD1WsmT', 'fqpcD1WsmT');
+$con= mysqli_connect("remotemysql.com", "fqpcD1WsmT", "Xt9SLaf4VJ" ,"fqpcD1WsmT");
 
 ?>
